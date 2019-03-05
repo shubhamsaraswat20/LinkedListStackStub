da918bcf9f6e84ca14f6a240fab219c1aefa1436
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2019-03-05 12:52:03 +0530

    class:Node; add constructor for both attributes

--- src/main/Stack.java
+++ src/main/Stack.java
@@ -5,6 +5,11 @@ public class Stack {
   private static class Node {
     private int data;
     private Node next;
+
+    public Node(int data, Node next) {
+      this.data = data;
+      this.next = next;
+    }
   }
 
 }
